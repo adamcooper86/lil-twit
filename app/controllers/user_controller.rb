beb18dfa0ddf90ea122525b80ae811f14497ed6d
Marshall Hattersley & Chris <pair+mwhatters.yago580@devbootcamp.com>
2015-07-09 15:18:50 -0700

    add crud controller functionality for users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,6 +7,7 @@ get '/users/:id' do
   end
 end
 
+# create
 post '/users' do
   @user = User.create(username: params[:username],
               password: params[:password],
@@ -21,3 +22,34 @@ end
 get '/users/new' do
   erb :'users/new'
 end
+
+# read
+get '/users' do
+  @users = User.all
+  erb :all_users
+end
+
+#update
+put 'users/:id' do
+  @user = User.find(params[:id])
+  @user.update_attributes(username: params[:username],
+              password: params[:password],
+              first_name: params[:first_name],
+              last_name: params[:last_name],
+              email: params[:email],
+              city: params[:city],
+              state: params[:state]
+              )
+  redirect "/users/#{params[:id]}"
+end
+
+#delete
+delete '/users/:id' do
+  @user = User.find(current_user.id)
+  logout # log user out and then delete them from database
+  User.destroy(@user.id)
+  redirect '/'
+end
+
+
+
