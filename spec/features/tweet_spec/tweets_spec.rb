cdddb4771ea4f686b5272851685aa6d06882234a
dannyshafer <github@danielshafer.com>
2015-07-09 12:24:24 -0700

    wrote test for existence of database table

--- spec/features/tweet_spec/tweets_spec.rb
+++ spec/features/tweet_spec/tweets_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe create_tweets do
-  it "creates a database" do
-    tweet_table = db_connection.table_exist?(:create_tweets)
-    expect(tweet_table).to be_truthy
+describe "create_tweets" do
+  it "creates a new tweet" do
+
+    expect { Tweet.create(user_id: 1, original_tweet_id: 1, content: "this tweet is amazing!") }.to change{Tweet.count}.by(1)
   end
 end
